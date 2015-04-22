8ed47618123a3298664d60bab4ae563759035e37
trezona-lecomte <trezona.lecomte@gmail.com>
2015-04-22 22:58:19 +1200

    Remove name uniqueness validation

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,3 @@
 class Album < ActiveRecord::Base
-  validates :name, presence: true,
-                   uniqueness: { case_sensitive: false }
+  validates :name, presence: true
 end
--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -12,11 +12,4 @@ RSpec.describe Album, type: :model do
     expect(album.valid?).to be true
   end
 
-  it "has a unique name" do
-    radiohead = Album.create(name: "Radiohead")
-    imposter = Album.create(name: "radiohead")
-    expect(radiohead.valid?).to be true
-    expect(imposter.valid?).to be false
-  end
-
 end
