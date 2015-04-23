ecaab71ac5c88d52e9482a02c52a1caac5ef6a10
trezona-lecomte <trezona.lecomte@gmail.com>
2015-04-23 23:12:32 +1200

    Make tracks belong to artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,6 @@
 class Artist < ActiveRecord::Base
   has_many :albums
+  has_many :tracks
   validates :name, presence: true,
                    uniqueness: { case_sensitive: false }
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Artist, type: :model do
 
   it { should validate_presence_of(:name) }
+  it { should have_many(:tracks) }
 
   it "is valid after creation" do
     artist = build(:artist)
