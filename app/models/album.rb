58ad418e3e515f10001aa3b113a50916c9c68317
trezona-lecomte <trezona.lecomte@gmail.com>
2015-04-23 22:28:21 +1200

    Add album/artist association

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,4 @@
 class Album < ActiveRecord::Base
+  belongs_to :artist
   validates :name, presence: true
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,5 @@
 class Artist < ActiveRecord::Base
+  has_many :albums
   validates :name, presence: true,
                    uniqueness: { case_sensitive: false }
 end
