56c6ea4185a2f01ee35efb32231c62bc54bd42eb
trezona-lecomte <trezona.lecomte@gmail.com>
2015-04-23 23:03:51 +1200

    Validate presence of track title

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,4 +1,5 @@
 class Track < ActiveRecord::Base
   belongs_to :artist
   belongs_to :album
+  validates :title, presence: true
 end
--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Track, type: :model do
 
   it { should validate_presence_of(:title) }
+  it { should belong_to(:album) }
+  it { should belong_to(:artist) }
 
 end
