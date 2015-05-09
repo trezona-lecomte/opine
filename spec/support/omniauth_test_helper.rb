97724c0d75d5d4546f224f52e7bc2e4b145152f5
trezona-lecomte <trezona.lecomte@gmail.com>
2015-05-09 15:33:40 +1200

    Fix mock_auth for digitalocean

--- spec/support/omniauth_test_helper.rb
+++ spec/support/omniauth_test_helper.rb
@@ -2,9 +2,13 @@ module OmniauthTestHelper
   def valid_digitalocean_login_setup
     if Rails.env.test?
       OmniAuth.config.test_mode = true
-      OmniAuth.config.add_mock(:digitalocean, {
-        :uid => '12345',
-        :email => 'test@example.com'
+      OmniAuth.config.mock_auth[:digitalocean] = OmniAuth::AuthHash.new({
+        provider: 'digitalocean',
+        uid: '12345',
+        info: {
+          'name' => 'Kieran Trezona-le Comte',
+          'email' => 'kieran@trezona.com'
+        }
       })
     end
   end
