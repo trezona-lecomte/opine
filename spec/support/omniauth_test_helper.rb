698d5619dc1001459ff38b0464dc8a7a3ae00370
trezona-lecomte <trezona.lecomte@gmail.com>
2015-05-10 19:16:09 +1200

    Add spec for google omniauth

--- spec/support/omniauth_test_helper.rb
+++ spec/support/omniauth_test_helper.rb
@@ -26,4 +26,32 @@ module OmniauthTestHelper
       })
     end
   end
+
+  def valid_facebook_login_setup
+    if Rails.env.test?
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
+        provider: 'facebook',
+        uid: '34567',
+        info: {
+          name: 'Freddy Facebook',
+          email: 'freddy@facebook.com'
+        }
+      })
+    end
+  end
+
+  def valid_google_login_setup
+    if Rails.env.test?
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
+        provider: 'google',
+        uid: '45678',
+        info: {
+          name: 'Greg Google',
+          email: 'greg@google.com'
+        }
+      })
+    end
+  end
 end
