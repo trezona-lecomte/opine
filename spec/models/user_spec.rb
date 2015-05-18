9c7f2887907642e0b33377cae6ce9b9ba476181d
trezona-lecomte <trezona.lecomte@gmail.com>
2015-05-18 22:17:00 +1200

    Spec user emails present & unique

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 describe User, type: :model do
 
   it { should validate_presence_of :name }
+  it { should validate_presence_of :email }
+  it { should validate_uniqueness_of :email }
+  it { should have_many :comments }
 
   before :each do
     @user = create :user
