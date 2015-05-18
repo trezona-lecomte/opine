68d5e57705607bfe34135cab26881d562a9085c5
trezona-lecomte <trezona.lecomte@gmail.com>
2015-05-18 22:01:26 +1200

    Spec user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, type: :model do
+
+  it { validate_presence_of :name }
+
+  before :each do
+    @user = create :user
+  end
+
+  it 'is valid' do
+    expect(@user).to be_valid
+  end
 end
