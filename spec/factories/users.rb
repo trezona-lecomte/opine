a93e2e83af8bdb4640b8b9e0898f513ea7e70304
trezona-lecomte <trezona.lecomte@gmail.com>
2015-05-18 21:50:41 +1200

    Add Name to Users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,18 @@
 FactoryGirl.define do
   factory :user do
-    
+    name Faker::Name.name
+    email Faker::Internet.email
+    password Faker::Internet.password
+
+    factory :user_with_comments do
+      transient do
+        comments_count 5
+      end
+
+      after(:create) do |user, evaluator|
+        create_list(:comment, evaluator.comments_count, user: user)
+      end
+    end
   end
 
 end
