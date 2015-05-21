631e8ed785ec37d374dd45804ea7921e30bdbe5c
trezona-lecomte <trezona.lecomte@gmail.com>
2015-05-21 17:15:51 +1200

    Tidy comment spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Comment, type: :model do
   
   it { should validate_presence_of :text }
+  it { should belong_to :user }
 
   before :each do
     @comment = build :comment
@@ -10,9 +11,5 @@ describe Comment, type: :model do
 
   it 'is valid' do
     expect(@comment).to be_valid
-  end
-
-  it 'belongs to a user' do
-    expect(@comment.user).to be_valid
   end
 end
