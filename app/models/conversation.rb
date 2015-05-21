f3cff2f0ac1fe44f70f50875e9c6695adc9225f2
trezona-lecomte <trezona.lecomte@gmail.com>
2015-05-21 20:01:31 +1200

    Add name to conversations

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,4 +1,5 @@
 class Conversation < ActiveRecord::Base
   belongs_to :group
   has_many :comments
+  validates :name, presence: true
 end
--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -4,4 +4,5 @@ describe Conversation, type: :model do
   
   it { should have_many :comments }
   it { should belong_to :group }
+  it { should validate_presence_of :name }
 end
