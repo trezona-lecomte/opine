1cfdc7008922a17f04bb2ecb5dd0d4ccb34ed3b7
trezona-lecomte <trezona.lecomte@gmail.com>
2015-05-30 12:18:06 +1200

    Add basic functions to conversations

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,4 +3,35 @@ class ConversationsController < ApplicationController
   def index
     @conversations = Conversation.all
   end
+
+  def new
+    @conversation = Conversation.new
+  end
+
+  def show
+    set_conversation
+  end
+
+  def create
+    @conversation = Conversation.new(conversation_params)
+    respond_to do |format|
+      if @conversation.save
+        format.html { redirect_to @conversation, notice: 'conversation was successfully created.' }
+        format.json { render :show, status: :created, location: @conversation }
+      else
+        format.html { render :new }
+        format.json { render json: @conversation.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_conversation
+      @conversation = Conversation.find(params[:id])
+    end
+
+    def conversation_params
+      params.require(:conversation).permit(:name)
+    end
 end
