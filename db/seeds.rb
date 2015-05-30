99a916dccb5543c8c980c6c4bad3dd06e776a9b8
trezona-lecomte <trezona.lecomte@gmail.com>
2015-05-30 16:16:02 +1200

    Add seeds for dev db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "admin",
+             email: "admin@opine.org",
+             password:              "foobar123",
+             password_confirmation: "foobar123")
+99.times do  |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@opine.org"
+  password = "password123"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
+
+5.times do |n|
+  name = "#{Faker::Lorem.sentence} conversation"
+  Conversation.create!(name: name)
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  conversation = Conversation.find(rand(1..5))
+  users.each { |user| user.comments.create!(text: content,
+                                            conversation: conversation) }
+end
+
