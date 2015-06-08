b28dba981a3a1278ea394b5120789a35316b638e
Kieran Trezona-le Comte <trezona.lecomte@gmail.com>
2015-06-08 21:11:54 +1200

    Add artist_with_albums factory

--- spec/factories/albums.rb
+++ spec/factories/albums.rb
@@ -1,6 +1,17 @@
 FactoryGirl.define do
+
   factory :album do
     name "Amnesiac"
     artist
+
+    factory :album_with_tracks do
+      transient do
+        tracks_count 5
+      end
+
+      after(:create) do |album, evaluator|
+        create_list(:track, evaluator.tracks_count, album: album)
+      end
+    end
   end
 end
--- spec/factories/tracks.rb
+++ spec/factories/tracks.rb
@@ -1,9 +1,8 @@
 FactoryGirl.define do
   factory :track do
     title "MyString"
-artist nil
-album nil
-length 1
+    artist nil
+    album nil
+    length 1
   end
-
 end
